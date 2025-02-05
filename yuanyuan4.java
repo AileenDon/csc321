61f752a94ffb39ce53d5a2bf4bfe1902f3d3d523
Aileen Dong <120889846+AileenDon@users.noreply.github.com>
2025-02-04 18:32:57 -0800

    Update yuanyuan4.java

--- yuanyuan4.java
+++ yuanyuan4.java
@@ -6,16 +6,34 @@ public class yuanyuan4
 {
     public static void main(String[] args) 
     {
-        int x=0;
-        System.out.println("choose an option");
-	System.out.println("option 1");
-	System.out.println("option 2");
-	System.out.println("option 3");
-	System.out.println("option 4");
-        Scanner userinput= new Scanner(System.in);
-	
-	x=userinput.nextInt();
- 	System.out.println("you chose option "+ x);
+	Scanner userinput = new Scanner(System.in);
+        int x = 0;
+        
+        while (true) 
+	{
+            System.out.print("Enter your choice: ");
+            
+            x = userinput.nextInt();
+            
+            switch (x) 
+	    {
+                case 1:
+                    System.out.println("option 1");
+                    break;
+                case 2:
+                    System.out.println("option 2");
+                    break;
+                case 3:
+                    System.out.println("option 3");
+                    break;
+                case 4:
+                    System.out.println("option 4");
+                    userinput.close();
+                    return;
+                default:
+                    System.out.println("Invalid choice.");
+            }
+            System.out.println();
     }
 }
 
