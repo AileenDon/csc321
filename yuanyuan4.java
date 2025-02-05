bf7964a5373a2b7971993b2b422db59cb277aa7f
AileenDon <yuanyuan.dong2000@gmail.com>
2025-02-05 02:21:15 +0000

    menu version 1

---
+++ yuanyuan4.java
@@ -0,0 +1,21 @@
+// yuanyuan4
+
+import java.util.Scanner;
+
+public class yuanyuan4 
+{
+    public static void main(String[] args) 
+    {
+        int x=0;
+        System.out.println("choose an option");
+	System.out.println("option 1");
+	System.out.println("option 2");
+	System.out.println("option 3");
+	System.out.println("option 4");
+        Scanner userinput= new Scanner(System.in);
+	
+	x=userinput.nextInt();
+ 	System.out.println("you chose option "+ x);
+    }
+}
+
